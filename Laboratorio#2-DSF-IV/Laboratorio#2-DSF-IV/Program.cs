db9afaeb49d4a1f35a63993f14da69a606995bf8
CESAR <eliascastillo2509@gmail.com>
2024-09-03 13:34:48 -0500

    Se creo el lab 2, con los temas respectosd de variables.

---
+++ Laboratorio#2-DSF-IV/Laboratorio#2-DSF-IV/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Cliente cliente = new Cliente();
+        cliente.FirstName = "Cesar";
+        cliente.LastName = "Castillo";
+        cliente.Age = 22;
+        cliente.Id = 1;
+
+        Console.WriteLine(cliente.GetFullName());
+    }
+
+
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName + " " + Age;
+        }
+    }
+
+
+    //Definicion de metodos que suma dos números e imprimir resultado.
+
+    int dato = 54;
+    int dato2 = 75;
+    public void Suma(int dato1, int dato2)
+    {
+        int valLocal = dato1 + dato2;
+        Console.WriteLine(valLocal);
+    }
+
+
+}
---
+++ Laboratorio#2-DSF-IV/Laboratorio21/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        //Asignacion a la variables estatica 
+        Myclass.valor = 44;
+        Myclass.valor2 = 0;
+
+        int valor = 65;
+        int valor2 = valor;
+        valor = 28;
+
+        Console.WriteLine(Myclass.valor);
+        Console.WriteLine(Myclass.valor2);
+
+        Console.WriteLine(valor);
+        Console.WriteLine(valor2);
+
+
+        //Nueva Instancia
+        Myclass obj1 = new Myclass();
+        obj1.FirstName = "Lewis";
+        obj1.Age = 30;
+        //Asignacion de una variable a otra 
+        Myclass obj2 = obj1;
+        //Cambioen la propiedad afectada tanto el obj1 con obj2
+        obj2.FirstName = "Russell";
+
+        Console.WriteLine(obj1.FirstName);
+        Console.WriteLine(obj2.FirstName);
+    }
+
+    public class Myclass
+    {
+        //Declaracion de variable estatica
+        public static int valor;
+        public static int valor2;
+
+        public string FirstName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + Age;
+        }
+    }
+
+
+}
