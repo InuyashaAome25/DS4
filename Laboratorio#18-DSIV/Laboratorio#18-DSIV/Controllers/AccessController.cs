63db806933141121b6884f5a4fc2f35ad66089fb
CESAR <eliascastillo2509@gmail.com>
2024-11-06 21:00:56 -0500

    Subida de laboratorio n°18 - Solucion completa

---
+++ Laboratorio#18-DSIV/Laboratorio#18-DSIV/Controllers/AccessController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio_18_DSIV.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un error :("+ ex.Message);
+            }
+        }
+    }
+}
---
+++ Laboratorio#18-DSIV/Laboratorio#18-DSIV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laboratorio_18_DSIV.MvcApplication" Language="C#" %>
